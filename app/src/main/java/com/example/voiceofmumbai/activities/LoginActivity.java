b819671ad00d87efd84f2d437d366c8d250eeb57
roh-7 <rohit.6233@outlook.com>
2018-10-05 15:14:44 +0530

    init

---
+++ app/src/main/java/com/example/voiceofmumbai/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.voiceofmumbai.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.voiceofmumbai.R;
+
+public class LoginActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+}
