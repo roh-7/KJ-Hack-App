27042980d129e8e60600e607381bf6d9eb7f063f
Arvindraj <arvindultimate7352@gmail.com>
2018-10-06 09:42:28 +0530

    FeedItemDetails: added
    Signed-off-by: Arvindraj <arvindultimate7352@gmail.com>

---
+++ app/src/main/java/com/example/voiceofmumbai/activities/FeedItemDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.voiceofmumbai.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.voiceofmumbai.R;
+
+public class FeedItemDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_item_details);
+    }
+}
